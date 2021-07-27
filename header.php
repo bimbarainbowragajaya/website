c9d510885a7585a44c09c63ac52f3db24a93c8ec
bimbarainbowragajaya <87311310+bimbarainbowragajaya@users.noreply.github.com>
2021-07-27 16:05:40 +0700

    header.php
    header

--- header.php
+++ header.php
@@ -1,1 +1,69 @@
-aa
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Bimba Rainbow Kids Raga Jaya Mita - Template · Bootstrap v5.0</title>
+	 
+
+<link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/carousel/">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="https://bimbarainbowragajaya.github.io/website/LOGO-RAINBOW-png.png" sizes="180x180">
+<link rel="icon" href="https://bimbarainbowragajaya.github.io/website/LOGO-RAINBOW-png.png" sizes="32x32" type="image/png">
+<link rel="icon" href="https://bimbarainbowragajaya.github.io/website/LOGO-RAINBOW-png.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="https://bimbarainbowragajaya.github.io/website/LOGO-RAINBOW-png.png">
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/5.0/examples/carousel/carousel.css" rel="stylesheet">
+  </head>
+  <body>
+	  
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+    <div class="container-fluid">
+      <img class="bd-placeholder-img" style="max-width:80px" src="https://bimbarainbowragajaya.github.io/website/LOGO-RAINBOW-png.png" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+      <a class="navbar-brand" href="#">Bimba Raga Jaya</a>
+      <form class="d-flex">
+	  <a href="http://wa.me/6289675877106?text=Halo%20bimba%20rainbow%20kids%20Raga%20Jaya,%20Saya%20ingin%20mendaftarkan%20anak%20saya%20di%20bimba%20rainbow%20kids%20Raga%20Jaya." target="_blank" rel="nofollow" halo="">
+	  <img alt="button-wa" data-src="https://bimbarainbowragajaya.github.io/website/69bd1668.png" class="attachment-large size-large lazyloaded" src="https://bimbarainbowragajaya.github.io/website/69bd1668.png" style="max-width:50px"></a>
+          <font color="#FFFFFF"><b>Tanya</b></font> 
+	</form>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </nav>
+</header>
+	 
